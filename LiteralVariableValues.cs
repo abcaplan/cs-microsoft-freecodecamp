23316f07dbf5babbf74c47d81bd78fadbb6c83bb
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-02-12 14:38:09 +0000

    Add 2nd Microsoft chapter.

---
+++ LiteralVariableValues.cs
@@ -0,0 +1,11 @@
+string userName = "Bob";
+int messages = 3;
+decimal degrees = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(userName);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(degrees);
+Console.Write(" celsius.");
