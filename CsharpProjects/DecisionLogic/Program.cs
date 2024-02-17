db257b2dce8b9aae2a3f936608b34e27b74dd427
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-02-17 22:04:21 +0000

    Add Decision Logic program for dealing dmg with a heavy sword for dnd 5e

---
+++ CsharpProjects/DecisionLogic/Program.cs
@@ -0,0 +1,10 @@
+﻿// DND dice roll for heavy weapon that does 1d12 bludgeoning, 1d6 bleeding, 1d4 radiance
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 13);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 5);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice rolls: {roll1} bludgeoning, {roll2} bleeding and {roll3} radiance damage for a total of {total}.");
