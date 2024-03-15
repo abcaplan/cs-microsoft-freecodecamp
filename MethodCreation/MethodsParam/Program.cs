8dc2ed110cbc876acc80060ad68fa0e002e53e9e
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-03-15 19:35:04 +0000

    Add method with parameters practice.

---
+++ MethodCreation/MethodsParam/Program.cs
@@ -0,0 +1,146 @@
+﻿// int[] schedule = { 800, 1200, 1600, 2000 };
+// 
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+// {
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//     {
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     }
+//     else
+//     {
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     }
+// 
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
+//     }
+// }
+// 
+// DisplayAdjustedTimes(schedule, 6, -6);
+
+
+
+// int[] array = { 1, 2, 3, 4, 5 };
+// 
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
+// 
+// void PrintArray(int[] array)
+// {
+//     foreach (int a in array)
+//     {
+//         Console.Write($"{a} ");
+//     }
+//     Console.WriteLine();
+// }
+// 
+// void Clear(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = 0;
+//     }
+// }
+
+
+
+// string status = "Healthy";
+// 
+// Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// Console.WriteLine($"End: {status}");
+// 
+// void SetHealth(bool isHealthy)
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
+
+
+
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
+// 
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+// ShowRSVPs();
+// 
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// {
+//     if (inviteOnly)
+//     {
+//         bool found = false;
+//         foreach (string guest in guestList)
+//         {
+//             if (guest.Equals(name))
+//             {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         if (!found)
+//         {
+//             Console.WriteLine($"Sorry, {name} is not on the guest list");
+//             return;
+//         }
+//     }
+// 
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
+// }
+// 
+// void ShowRSVPs()
+// {
+//     Console.WriteLine("\nTotal RSVPs:");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine(rsvps[i]);
+//     }
+// }
+
+
+
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(firstName: corporate[i, 0], lastName: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(firstName: corporate[i, 0], lastName: corporate[i, 1], domain: externalDomain);
+}
+
+void DisplayEmail(string firstName, string lastName, string domain = "contoso.com")
+{
+    string email = firstName.Substring(0, 2) + lastName;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
