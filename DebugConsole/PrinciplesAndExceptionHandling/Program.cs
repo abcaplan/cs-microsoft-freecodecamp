5a860969da507826423e1fd647de261efad3f39a
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-03-20 19:49:13 +0000

    Add debug files. Currently Debug works on PrinciplesAndExceptionHandling folder. Debug101 does not work in current folder (works separately)

---
+++ DebugConsole/PrinciplesAndExceptionHandling/Program.cs
@@ -0,0 +1,7 @@
+﻿// Source: https://learn.microsoft.com/en-us/training/modules/review-principles-code-debugging-exception-handling-c-sharp/2-get-started-test-debug-exception
+
+string[] students = ["Sophia", "Nicolas", "Zahirah", "Jeong"];
+
+int studentCount = students.Length;
+
+Console.WriteLine("The final name is: " + students[studentCount]);
