ba9cbb07aa70e97c6dae35e3adf1492973cb732e
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-05-23 15:03:27 +0100

    Add find unpaired number within pair of elements within an array using XOR gates

---
+++ Codility/FindUnpairedNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://app.codility.com/c/run/trainingRWEYZH-A9H/
+
+/* A non-empty array A consisting of N integers is given.
+** The array contains an odd number of elements, and each element of the array
+** can be paired with another element that has the same value, 
+** except for one element that is left unpaired.
+*/
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        int result = 0;
+
+        foreach (int num in A)
+        {
+            // XOR gates
+            result ^= num;
+        }
+
+        return result;
+    }
+}
