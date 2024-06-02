20e55f4d6c79f217a15796072d074c0b977fe243
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-06-02 17:43:49 +0100

    Add Leetcode folder with AddTwoNumber exercise

--- TestingGrounds/Program.cs
+++ TestingGrounds/Program.cs
@@ -1,6 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
 
-public class Solution
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(string S)
+    {
+        // Store the number of appearances of all characters in a dictionary
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        // Count the appearances
+        foreach (char c in S)
+        {
+            if (charCount.ContainsKey(c))
+            {
+                charCount[c]++;
+            }
+            else
+            {
+                charCount[c] = 1;
+            }
+        }
+
+        // Count the number of characters with odd appearances
+        int oddCount = 0;
+        foreach (int count in charCount.Values)
+        {
+            if (count % 2 != 0)
+            {
+                oddCount++;
+            }
+        }
+
+        // A palindrome can only have a maximum of one odd appearance of a character
+        // Calculate characters to be removed to form a palindrome
+        int charRemoved;
+        if (oddCount > 1)
+        {
+            charRemoved = oddCount - 1;
+        }
+        else
+        {
+            charRemoved = 0;
+        }
+
+        return charRemoved;
+    }
+}
+
+using System;
+
+class Solution
+{
+    public void solution(int N)
+    {
+        bool enable_print = false;
+        int result = 0;
+
+        while (N > 0)
+        {
+            int last_number = N % 10;
+            if (last_number != 0)
+            {
+                enable_print = true;
+            }
+            if (enable_print)
+            {
+                result = result * 10 + last_number;
+            }
+            N = N / 10;
+        }
+    }
+}
+
+/*public class Solution
 {
     public int solution(int[] A)
     {
@@ -26,7 +103,7 @@ public class Solution
 
         return arrayLength + 1;
     }
-}
+} */
 
 
 /*
