fca7d04bf2655fe0f8f08e807706c3f536816d2c
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-06-02 18:45:54 +0100

    Add 344ReverseString exercise

---
+++ Leetcode/344ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int leftPos = 0;
+        int rightPos = s.Length - 1;
+
+        while (leftPos < rightPos)
+        {
+            char temp = s[leftPos];
+            s[leftPos] = s[rightPos];
+            s[rightPos] = temp;
+
+            leftPos++;
+            rightPos--;
+        }
+    }
+}
