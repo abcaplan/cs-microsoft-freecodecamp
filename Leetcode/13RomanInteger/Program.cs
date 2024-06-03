90836cdbd21461cee4d6f8edfcf44f607a3f76e4
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-06-03 18:28:06 +0100

    Add roman number to integer number exercise

---
+++ Leetcode/13RomanInteger/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        int number = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            // Check if this character is less than the next character
+            if (i < s.Length - 1 && GetValue(s[i]) < GetValue(s[i + 1]))
+            {
+                number -= GetValue(s[i]);
+            }
+            else
+            {
+                number += GetValue(s[i]);
+            }
+        }
+        return number;
+    }
+
+    private int GetValue(char romanChar)
+    {
+        switch (romanChar)
+        {
+            case 'M':
+                return 1000;
+            case 'D':
+                return 500;
+            case 'C':
+                return 100;
+            case 'L':
+                return 50;
+            case 'X':
+                return 10;
+            case 'V':
+                return 5;
+            case 'I':
+                return 1;
+            default:
+                throw new ArgumentException("Invalid Roman numeral character");
+        }
+    }
+}
