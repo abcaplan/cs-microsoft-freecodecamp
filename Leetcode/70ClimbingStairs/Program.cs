9ea17435299856df78f7cc2b6a539e2db8f095fa
Bogdan Alexandru Caplan <53822730+abcaplan@users.noreply.github.com>
2024-06-03 15:41:11 +0100

    Add climbing stairs exercise using 1 or 2 steps at a time

---
+++ Leetcode/70ClimbingStairs/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    // Similar to Fibonacci
+    public int ClimbStairs(int n)
+    {
+        // Base cases
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        // Initialize the variables for the base cases
+        int oneStepBefore = 2;
+        int twoStepsBefore = 1;
+        int allWays = 0;
+
+        // Compute the number of ways iteratively
+        for (int i = 3; i <= n; i++)
+        {
+            allWays = oneStepBefore + twoStepsBefore;
+            twoStepsBefore = oneStepBefore;
+            oneStepBefore = allWays;
+        }
+
+        return allWays;
+    }
+}
